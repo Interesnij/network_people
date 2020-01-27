fe7aa817dd5a3e8a0fae76cb2685e8c5da3971a3
Serg <ochkarik1983@mail.ru@>
2020-01-27 19:52:54 +0300

    G

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.views.generic.base import TemplateView
 
 
 class MainPageView(TemplateView):
-	template_name="main/mainpage2.html"
+	template_name="main/mainpage.html"
 
 	def get_context_data(self,**kwargs):
 		context=super(MainPageView,self).get_context_data(**kwargs)
