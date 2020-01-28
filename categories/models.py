94dbbfd40d493f35991d4e7015d6f541379952a6
Serg <ochkarik1983@mail.ru@>
2020-01-28 17:10:25 +0300

    J

--- categories/models.py
+++ categories/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
 	name=models.CharField(max_length=100,db_index=True,unique=True,verbose_name="Название")
-    slug=models.CharField(max_length=100,db_index=True,unique=True,verbose_name="Название для url")
+	slug=models.CharField(max_length=100,db_index=True,unique=True,verbose_name="Название для url")
 	image=models.ImageField(null=True,blank=True,upload_to="blog_subcategories/list")
 	order=models.PositiveSmallIntegerField(default=0,db_index=True,verbose_name="Порядковый номер")
 	def __str__(self):
