2e0af422383bcac6397fa5a2acf95376383283c2
Serg <ochkarik1983@mail.ru@>
2020-02-03 12:31:03 +0300

    J

--- templates/generic/main_intro.js
+++ templates/generic/main_intro.js
@@ -1,43 +1,4 @@
 
-(function epicHello() {
-    var userAgent = navigator.userAgent.toLowerCase();
-    var supported = /(chrome|firefox)/;
-
-    if (supported.test(userAgent.toLowerCase())) {
-        var dark = [
-            'padding: 20px 5px 16px',
-            'background-color: #171718',
-            'color: #bc994e',
-        ].join(';');
-
-        if (userAgent.indexOf('chrome') > -1) {
-            dark += ';';
-            dark += [
-                'padding: 20px 5px 16px 40px',
-                'background-image: url(/static/images/logo.png)',
-                'background-position: 10px 9px',
-                'background-repeat: no-repeat',
-                'background-size: 26px 30px',
-            ].join(';');
-        }
-
-        var gold = [
-            'padding: 20px 5px 16px',
-            'background-color: #bc994e',
-            'color: #ffffff',
-        ].join(';');
-
-        var spacer = [
-            'background-color: transparent',
-        ].join(';');
-
-        var msg = '\n\n %c Исполнил ♥︎ Дарт Вейдер %c вот так вот %c \n\n\n';
-
-        console.log(msg, dark, gold, spacer);
-    } else if (window.console) {
-        console.log('Исполнил ♥︎ Дарт Вейдер - вот так вот');
-    }
-}());
 
 ! function(e) {
     function t(t) {
