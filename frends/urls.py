bd28f6c6a3d622b30f27382584bf141126ecf6d0
Serg <ochkarik1983@mail.ru@>
2020-02-09 09:58:42 +0300

    J

--- frends/urls.py
+++ frends/urls.py
@@ -4,5 +4,5 @@ from frends.views import FrendListView, FrendDetailView
 
 urlpatterns = [
     url(r'^(?P<slug>[\w\-]+)/$', FrendListView.as_view()),
-    url(r'^(?P<frend_slug>[\w\-]+)/$', FrendDetailView.as_view(), name="frend_detail"),
+    url(r'^(?P<frend_slug>[\w\-]+)/detail/$', FrendDetailView.as_view(), name="frend_detail"),
 ]
--- peoples/urls.py
+++ peoples/urls.py
@@ -4,5 +4,5 @@ from peoples.views import PeopleListView, PeopleDetailView
 
 urlpatterns = [
     url(r'^(?P<slug>[\w\-]+)/$', PeopleListView.as_view()),
-    url(r'^(?P<people_slug>[\w\-]+)/$', PeopleDetailView.as_view(), name="people_detail"),
+    url(r'^(?P<people_slug>[\w\-]+)/detail/$', PeopleDetailView.as_view(), name="people_detail"),
 ]
